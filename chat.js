999de1fccc113624a93285cacb1bb6790dcf95da
VMLMS <shivammishra.id@gmail.com>
2024-10-07 16:38:32 +0530

    Descriptive message about the changes

--- chat.js
+++ chat.js
@@ -1,11 +1,12 @@
 // chat.js
 
 // When the chat icon is clicked, display the chatbox
-document.getElementById('chatIcon').addEventListener('click', function () {
-    document.getElementById('chatBox').style.display = 'block';
+// Chatbox toggle
+document.getElementById("chatIcon").addEventListener("click", function () {
+    document.getElementById("chatBox").style.display = "block";
 });
 
-// When the close button in the chatbox is clicked, hide the chatbox
-document.getElementById('closeChat').addEventListener('click', function () {
-    document.getElementById('chatBox').style.display = 'none';
+document.getElementById("closeChat").addEventListener("click", function () {
+    document.getElementById("chatBox").style.display = "none";
 });
+
